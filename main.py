d9b92dd428fbcea290691e88153857b24b87861e
EdgarNg1024 <Edgarng_1024@163.com>
2017-03-03 17:37:59 +0800

    找到了一个方法,能够找到小选择框

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import squares
 
 # 构建命令行参数解析并分析参数
 # 对应使用方式 python test_grader.py --image images/test_01.png
@@ -48,45 +49,51 @@ if len(cnts) > 0:
 paper = four_point_transform(image, docCnt.reshape(4, 2))
 warped = four_point_transform(gray, docCnt.reshape(4, 2))
 
-# 对灰度图应用大津二值化算法
-thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-# 在二值图像中查找轮廓，然后初始化题目对应的轮廓列表
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-questionCnts = []
-
-# 对每一个轮廓进行循环处理
-for q, c in enumerate(cnts):
-
-    # 计算轮廓的边界框，然后利用边界框数据计算宽高比
-    (x, y, w, h) = cv2.boundingRect(c)
-    ar = w / float(h)
-    # print 'w' + str(w), 'h' + str(h)
-
-    # 为了辨别一个轮廓是一个方框，要求它的边界框不能太小，在这里边至少是40个像素，而且它的宽高比要近似于1
-    # 为了去掉外面的那个方框,所以轮廓不能太长,小于300像素
-    # 轮廓肯定会有4个点
-    if ar >= 0.9 and 30 < w < 300:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) >= 4:
-            questionCnts.append(c)
-            # colorpoint = 255
-            # color = ((q % 3 & 01) * colorpoint, ((q + 1) % 3 & 01) * colorpoint, ((q + 2) % 3 & 01) * colorpoint)
-            # cv2.drawContours(paper, [c], -1, color, 1)
-
-# 以从顶部到底部的方法将我们的气泡轮廓进行排序，然后初始化正确答案数的变量。
-questionCnts = contours.sort_contours(questionCnts,
-                                      method="top-to-bottom")[0]
-
-for (q, i) in enumerate(questionCnts):
-    colorpoint = 255
-    color = ((q % 3 & 01) * colorpoint, ((q + 1) % 3 & 01) * colorpoint, ((q + 2) % 3 & 01) * colorpoint)
-    cv2.drawContours(paper, [questionCnts[q]], -1, color, 2)
-    # cv2.imshow("exam" + str(q), paper)
+# # 对灰度图应用大津二值化算法
+# thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+#
+# # 在二值图像中查找轮廓，然后初始化题目对应的轮廓列表
+# cnts = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+# questionCnts = []
+#
+# # 对每一个轮廓进行循环处理
+# for q, c in enumerate(cnts):
+#
+#     # 计算轮廓的边界框，然后利用边界框数据计算宽高比
+#     (x, y, w, h) = cv2.boundingRect(c)
+#     ar = w / float(h)
+#     # print 'w' + str(w), 'h' + str(h)
+#
+#     # 为了辨别一个轮廓是一个方框，要求它的边界框不能太小，在这里边至少是40个像素，而且它的宽高比要近似于1
+#     # 为了去掉外面的那个方框,所以轮廓不能太长,小于300像素
+#     # 轮廓肯定会有4个点
+#     if ar >= 0.9 and 30 < w < 300:
+#         peri = cv2.arcLength(c, True)
+#         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+#         if len(approx) >= 4:
+#             questionCnts.append(c)
+#             # colorpoint = 255
+#             # color = ((q % 3 & 01) * colorpoint, ((q + 1) % 3 & 01) * colorpoint, ((q + 2) % 3 & 01) * colorpoint)
+#             # cv2.drawContours(paper, [c], -1, color, 1)
+#
+# # 以从顶部到底部的方法将我们的气泡轮廓进行排序，然后初始化正确答案数的变量。
+# questionCnts = contours.sort_contours(questionCnts,
+#                                       method="top-to-bottom")[0]
+#
+# for (q, i) in enumerate(questionCnts):
+#     colorpoint = 255
+#     color = ((q % 3 & 01) * colorpoint, ((q + 1) % 3 & 01) * colorpoint, ((q + 2) % 3 & 01) * colorpoint)
+#     cv2.drawContours(paper, [questionCnts[q]], -1, color, 2)
+#     # cv2.imshow("exam" + str(q), paper)
 
 # cv2.imshow("Original", image)
 # cv2.imshow("exam", thresh)
-cv2.imshow("exam", paper)
+
+squares.find_squares(paper)
+squarespic = squares.find_squares(warped)
+cv2.drawContours(paper, squarespic, -1, (0, 0, 255), 2)
+cv2.imshow('squares', paper)
+
+# cv2.imshow("exam", gaussian)
 cv2.waitKey(0)
